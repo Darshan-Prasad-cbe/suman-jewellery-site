7872204227a8159ab5316fb9cf109dd7bd51c9b3
Darshan Prasad <darshanprasadcbe@gmail.com>
2025-09-17 03:15:52 +0530

    Prepare for GitHub Pages deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,44 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark custom-navbar shadow">
+    <nav className="navbar navbar-expand-lg navbar-dark custom-navbar shadow py-3">
       <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">Suman Jewellery</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+        {/* Brand */}
+        <Link className="navbar-brand fw-bold gold-glow" to="/">
+          Suman Jewellery
+        </Link>
+
+        {/* Hamburger toggle for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navMenu"
+          aria-controls="navMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
+
+        {/* Navbar links */}
         <div className="collapse navbar-collapse" id="navMenu">
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/products">Products</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/offers">Offers</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/reviews">Reviews</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/products">Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/offers">Offers</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/reviews">Reviews</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
           </ul>
         </div>
       </div>
